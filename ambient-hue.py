a1a3afcf01e67d4db093f545ce60b2c5385233a4
Luke Hutton <admin@combustion-studios.com>
2017-06-04 14:32:33 +0100

    start interface get

--- ambient-hue.py
+++ ambient-hue.py
@@ -2,6 +2,20 @@
 ambient hue
 (c) luke hutton 2017
 """
+import objc
+objc.loadBundle('CoreWLAN',
+       bundle_path='/System/Library/Frameworks/CoreWLAN.framework',
+       module_globals=globals())
+
+rooms = {}
 
 if __name__ == "__main__":
-    pass
+    for iname in CWInterface.interfaceNames():
+        interface = CWInterface.interfaceWithName_(iname)
+        rssi = interface.rssi()
+        break
+
+    print "ambient hue"
+    print "-----------"
+    print
+    print "Move to the first room you want to be able to control, then hit ENTER to start calibration. This only takes a few seconds per room."
