9eae71fc857ffb2095c8d4dcc32d802ade4bd45f
Luke Hutton <admin@combustion-studios.com>
2017-06-10 13:09:16 +0100

    capture rssi per room with 10 samples

--- ambient-hue.py
+++ ambient-hue.py
@@ -2,7 +2,13 @@
 ambient hue
 (c) luke hutton 2017
 """
+
+import numpy
 import objc
+import phue
+import time
+
+
 objc.loadBundle('CoreWLAN',
        bundle_path='/System/Library/Frameworks/CoreWLAN.framework',
        module_globals=globals())
@@ -10,6 +16,11 @@ objc.loadBundle('CoreWLAN',
 rooms = {}
 
 if __name__ == "__main__":
+    b = phue.Bridge(BRIDGE_IP)
+    b.get_api()
+    lights = b.get_light_objects('id')
+
+    interface = None
     for iname in CWInterface.interfaceNames():
         interface = CWInterface.interfaceWithName_(iname)
         rssi = interface.rssi()
@@ -18,4 +29,30 @@ if __name__ == "__main__":
     print "ambient hue"
     print "-----------"
     print
-    print "Move to the first room you want to be able to control, then hit ENTER to start calibration. This only takes a few seconds per room."
+    rooms = []
+    name = raw_input('Type a name for the first room you want to be able to control, then hit ENTER to start calibration. This only takes a few seconds per room.')  # noqa
+    measures = []
+    while(True):
+        measures.append(interface.rssi())
+        time.sleep(0.5)
+        if(len(measures) > 10):
+            break
+    new_room = {'rssi':measures,'name':name}
+    rooms.append(new_room)
+
+    while(True):
+        name = raw_input("Type a name for the next room you want to be able to control, then hit ENTER. Enter a blank name if you're done.")
+        if len(name) == 0:
+            break
+        measures = []
+        while(True):
+            measures.append(interface.rssi())
+            time.sleep(0.5)
+            if(len(measures) > 10):
+                break
+        new_room = {'rssi':measures,'name':name}
+        rooms.append(new_room)
+
+    print "Thanks! Here are the rooms you've captured:"
+    for room in rooms:
+        print "%s (%s dBm)" % (room['name'], numpy.mean(room['rssi']))
